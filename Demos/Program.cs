ac9fcaa390abb11980c7d35ea31e6b3ae5746708
robbinl <robbinl@telusplanet.net>
2021-09-30 14:43:42 -0600

    a

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,63 +1,65 @@
 ﻿using System;
 
-namespace loops
+namespace demos
 {
     class Program
     {
         static void Main(string[] args)
         {
             var app = new Program();
-            app.ForLoop();
+            app.App1("For Loop Simple Demo");
+            app.App2("For Loop ClassAve Demo");
+            app.App3("For Loop TotalSales Demo");
+
+            
            // return 0;
         }
-        #region X
-        private void X()
+        #region
+        private void App(string demoName)
         {
             try
             {
-                Console.WriteLine("X Demo started");
+                Console.WriteLine($"{demoName} started");
                 
-                Console.WriteLine("X Demo ended");
+                Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception in X: {ex.Message}");
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
             }
-            
         }
         #endregion
-        #region ForLoop
-        private void ForLoop()
+        #region
+        private void App1(string demoName)
         {
-            string methodName = "ForLoop Demo";
             try
             {
-                Console.WriteLine($"{methodName} started");
+                Console.WriteLine($"{demoName} started");
                 for (int i = 0; i < 10; i++)
                 {
                     Console.WriteLine("I will not sleep in class.");
                 }
-                Console.WriteLine($"{methodName} ended");
+                //throw new Exception("ok");
+                Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception in {methodName}: {ex.Message}");
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
             }
-            
         }
         #endregion
-        #region ForClassAve
-        private void ForClassAve()
+        #region
+        private void App2(string demoName)
         {
+            
             try
             {
-                Console.WriteLine("ForClassAve Demo started");
+                Console.WriteLine($"{demoName} started");
                 // Prompt and read number of students
                 Console.Write("Enter the number of students: ");
                 int numStudents = int.Parse(Console.ReadLine());
-
                 // For Each student, prompt and read in their mark
                 // Calculate a running total.
                 double sumOfMarks = 0;
@@ -73,31 +75,29 @@ namespace loops
                 avgMark = sumOfMarks / numStudents;
                 // Display average mark
                 Console.WriteLine($"Average Mark: {avgMark:n2}");
-                Console.WriteLine("ForClassAve Demo ended");
+                Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception in ForClassAve: {ex.Message}");
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
             }
-            
         }
         #endregion
-        #region ForTotalSales
-        private void ForTotalSales()
+        #region
+        private void App3(string demoName)
         {
             try
             {
-                Console.WriteLine("ForTotalSales Demo started");
+                Console.WriteLine($"{demoName} started");
                 
-                Console.WriteLine("ForTotalSales Demo ended");
+                Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception in ForTotalSales: {ex.Message}");
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
             }
-            
         }
         #endregion
     }
