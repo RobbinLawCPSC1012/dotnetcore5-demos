65bd18574f862053926827e01e2a24f44cc64bd8
robbinl <robbinl@telusplanet.net>
2021-09-30 15:08:42 -0600

    setup template

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -8,8 +8,18 @@ namespace demos
         {
             var loops = new Loops();
             loops.App1("For Loop Simple Demo");
-            loops.App2("For Loop ClassAve Demo");
-            loops.App3("For Loop TotalSales Demo");
+            loops.App2("For Loop Class Average Demo");
+            loops.App3("For Loop Total Sales Demo");
+            loops.App4("For Loop Class Average with High/Low Play Only Once");
+            loops.App5("For Loop Year End Balance");
+            loops.App6("For & While Loop Comparison");
+            loops.App7("While Loop Temperature");
+            loops.App8("While Loop Soccer Team");
+            loops.App9("While Loop Guess the Number Only Play Once");
+            loops.App10("Do While Simple Demo");
+            loops.App11("Do While Loop Class Average with High/Low Play Many Times");
+            loops.App12("Do While Loop Add Calculator Play Many Times");
+
 
             
         }
--- Demos/template.cs
+++ Demos/template.cs
@@ -2,35 +2,15 @@ using System;
 
 namespace demos
 {
-    class Loops
+    class Template
     {
-        #region
-        public void App(string demoName)
-        {
-            try
-            {
-                Console.WriteLine($"{demoName} started");
-                
-                Console.WriteLine($"{demoName} ended");
-                Console.WriteLine("");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
-            }
-        }
-        #endregion
         #region
         public void App1(string demoName)
         {
             try
             {
                 Console.WriteLine($"{demoName} started");
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine("I will not sleep in class.");
-                }
-                //throw new Exception("ok");
+                
                 Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
@@ -47,24 +27,7 @@ namespace demos
             try
             {
                 Console.WriteLine($"{demoName} started");
-                // Prompt and read number of students
-                Console.Write("Enter the number of students: ");
-                int numStudents = int.Parse(Console.ReadLine());
-                // For Each student, prompt and read in their mark
-                // Calculate a running total.
-                double sumOfMarks = 0;
-                double avgMark;
-                double curMark;
-                for (int i = 1; i <= numStudents; i++)
-                {
-                    Console.Write($"Enter mark for student #{i}: ");
-                    curMark = double.Parse(Console.ReadLine());
-                    sumOfMarks += curMark;
-                }
-                // Calculate average mark
-                avgMark = sumOfMarks / numStudents;
-                // Display average mark
-                Console.WriteLine($"Average Mark: {avgMark:n2}");
+                
                 Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
@@ -234,6 +197,5 @@ namespace demos
             }
         }
         #endregion
-
     }
 }
