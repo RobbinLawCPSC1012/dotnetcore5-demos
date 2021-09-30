e47ee816927def2fcd5f5cd1346c9d0f09c2445a
robbinl <robbinl@telusplanet.net>
2021-09-30 09:40:43 -0600

    start building demos

---
+++ aa-sequence/sequence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ cc-loops/loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
