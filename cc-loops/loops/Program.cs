7753f29f73cd6ec1c600c7e0b2a26a24beb8902f
robbinl <robbinl@telusplanet.net>
2021-09-30 09:55:58 -0600

    loop demos

--- cc-loops/loops/Program.cs
+++ cc-loops/loops/Program.cs
@@ -6,7 +6,26 @@ namespace loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new Program();
+            app.forloop();
+           // return 0;
         }
+        #region forloop
+        private void forloop()
+        {
+            try
+            {
+                Console.WriteLine("forloop Demo started");
+                
+                Console.WriteLine("forloop Demo ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in forloop: {ex.Message}");
+            }
+            
+        }
+        #endregion
     }
 }
