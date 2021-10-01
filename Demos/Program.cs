765d5d881970d578611dd3c50d3b791bc66e1e41
robbinl <robbinl@telusplanet.net>
2021-10-01 16:42:12 -0600

    Structure

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -7,37 +7,84 @@ namespace demos
         static void Main(string[] args)
         {
             var program = new Program();
+            program.Sequence();
+            program.Decision();
             //program.Loops();
-            program.Files();
+            //program.Files();
              
+        }
+        public void Sequence()
+        {
+            var apps = new Sequence();
+            apps.App1("1");
+            apps.App2("2");
+            apps.App3("3");
+            apps.App4("4");
+            apps.App5("5");
+            apps.App6("6");
+            apps.App7("7");
+            apps.App8("8");
+            apps.App9("9");
+            apps.App10("10");
+            apps.App11("11");
+            apps.App12("12");
+        }
+        public void Decision()
+        {
+            var apps = new Decision();
+            apps.App1("1");
+            apps.App2("2");
+            apps.App3("3");
+            apps.App4("4");
+            apps.App5("5");
+            apps.App6("6");
+            apps.App7("7");
+            apps.App8("8");
+            apps.App9("9");
+            apps.App10("10");
+            apps.App11("11");
+            apps.App12("12");
         }
         public void Loops()
         {
-            var loops = new Loops();
-            loops.App1("For Loop Simple Demo");
-            loops.App2("For Loop Class Average Demo");
-            loops.App3("For Loop Total Sales Demo");
-            loops.App4("For Loop Class Average with High/Low Play Only Once");
-            loops.App5("For Loop Year End Balance");
-            loops.App6("For & While Loop Comparison");
-            loops.App7("While Loop Temperature");
-            loops.App8("While Loop Soccer Team");
-            loops.App9("While Loop Guess the Number Only Play Once");
-            loops.App10("Do While Simple Demo");
-            loops.App11("Do While Loop Class Average with High/Low Play Many Times");
-            loops.App12("Do While Loop Add Calculator Play Many Times");
+            var apps = new Loops();
+            apps.App1("For Loop Simple Demo");
+            apps.App2("For Loop Class Average Demo");
+            apps.App3("For Loop Total Sales Demo");
+            apps.App4("For Loop Class Average with High/Low Play Only Once");
+            apps.App5("For Loop Year End Balance");
+            apps.App6("For & While Loop Comparison");
+            apps.App7("While Loop Temperature");
+            apps.App8("While Loop Soccer Team");
+            apps.App9("While Loop Guess the Number Only Play Once");
+            apps.App10("Do While Simple Demo");
+            apps.App11("Do While Loop Class Average with High/Low Play Many Times");
+            apps.App12("Do While Loop Add Calculator Play Many Times");
         }
         public void Files()
         {
-            var files = new Files();
-            files.App1();
-            files.App2("");
-            files.App3("");
-            files.App4("");
-            files.App5("5");
+            var apps = new Files();
+            apps.App1();
+            apps.App2("");
+            apps.App3("");
+            apps.App4("");
+            apps.App5("5");
         }
-        
-        
-        
+        // public void Decision()
+        // {
+        //     var apps = new Decision();
+        //     apps.App1("1");
+        //     apps.App2("2");
+        //     apps.App3("3");
+        //     apps.App4("4");
+        //     apps.App5("5");
+        //     apps.App6("6");
+        //     apps.App7("7");
+        //     apps.App8("8");
+        //     apps.App9("9");
+        //     apps.App10("10");
+        //     apps.App11("11");
+        //     apps.App12("12");
+        // }  
     }
 }
---
+++ Demos/abSequence.cs
@@ -0,0 +1,201 @@
+using System;
+
+namespace demos
+{
+    class Sequence
+    {
+        #region
+        public void App1(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App2(string demoName)
+        {
+            
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App3(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App4(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App5(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App6(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App7(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App8(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App9(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App10(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App11(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+        #region
+        public void App12(string demoName)
+        {
+            try
+            {
+                Console.WriteLine($"{demoName} started");
+                
+                Console.WriteLine($"{demoName} ended");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in {demoName}: {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
