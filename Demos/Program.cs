f8903e6b00518b5c4d94407bc02f48be187a7c85
robbinl <robbinl@telusplanet.net>
2021-11-23 13:01:00 -0700

    a

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -12,8 +12,8 @@ namespace Demos
             //program.Loops();
             //program.MethodsExceptions();
             //program.Arrays();
-            //program.Objects();
-            program.Files();
+            program.Objects();
+            //program.Files();
         }
         public void Sequence()
         {
@@ -155,27 +155,27 @@ namespace Demos
             var app1 = new Objects.App1();
             app1.App("1 - ");
             var app2 = new Objects.App2();
-            app2.App("2 - ");
+            //app2.App("2 - ");
             var app3 = new Objects.App3();
-            app3.App("3 - ");
+            //app3.App("3 - ");
             var app4 = new Objects.App4();
-            app4.App("4 - ");
+            //app4.App("4 - ");
             var app5 = new Objects.App5();
-            app5.App("5 - ");
+            //app5.App("5 - ");
             var app6 = new Objects.App6();
-            app6.App("6 - ");
+            //app6.App("6 - ");
             var app7 = new Objects.App7();
-            app7.App("7 - ");
+            //app7.App("7 - ");
             var app8 = new Objects.App8();
-            app8.App("8 - ");
+            //app8.App("8 - ");
             var app9 = new Objects.App9();
-            app9.App("9 - ");
+            //app9.App("9 - ");
             var app10 = new Objects.App10();
-            app10.App("10 - ");
+            //app10.App("10 - ");
             var app11 = new Objects.App11();
-            app11.App("11 - ");
+            //app11.App("11 - ");
             var app12 = new Objects.App12();
-            app12.App("12 - ");
+            //app12.App("12 - ");
         }
         public void Files()
         {
--- Demos/agObjects.cs
+++ Demos/agObjects.cs
@@ -1,16 +1,98 @@
 using System;
+using System.Collections.Generic;
 
 namespace Objects
 {
     #region
     public class App1
     {
+        private int GetIntBetweenMinMax(String msg, int min, int max)
+        {
+            bool inValidInput = true;
+            int num = 0;
+            while (inValidInput)
+            {
+                try
+                {
+                    Console.Write(msg);
+                    num = int.Parse(Console.ReadLine());
+                    if (num < min || num > max)
+                        throw new Exception($"Must be between {min} and {max}");
+                    inValidInput = false; 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid: {ex.Message}");
+                }
+            }
+            return num;
+        }
+
+        private const string SPECIALCHARACTERS = @",:;\/!?@#$%^&*~`0123456789";
+        private string GetString(String msg)
+        {
+            bool inValidInput = true;
+            string str = "";
+            while (inValidInput)
+            {
+                try
+                {
+                    Console.Write(msg);
+                    str = Console.ReadLine();
+                    foreach(char character in SPECIALCHARACTERS)
+                    if (str.Contains(character))
+                        throw new FormatException($"String contains an invalid character.");
+                    inValidInput = false; 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid: {ex.Message}");
+                }
+            }
+            return str;
+        }
+
+        private void WithArrays()
+        {
+            Console.WriteLine("*** Using traditional arrays where size must be statically set at declaration ***");
+            int studentCount = GetIntBetweenMinMax("How many students in your class? ", 0, 100);
+            string[] studentNames = new string[studentCount];
+            int[] studentGrades = new int[studentCount];
+            for (int i = 0; i < studentCount; i++)
+            {
+                studentNames[i] = GetString("Student Name: ");
+                studentGrades[i] = GetIntBetweenMinMax("Student Grade: ", 0, 100);
+            }
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine($"Name: {studentNames[i]}, Grade: {studentGrades[i]}");
+            }
+        }
+        private void WithLists()
+        {
+            Console.WriteLine("*** Using dynamic arrays called lists where size is not set at declaration ***");
+            List<string> studentNames = new List<string>();
+            List<int> studentGrades = new List<int>();
+            var adding = true;
+            while(adding)
+            {
+                studentNames.Add(GetString("Student Name: "));
+                studentGrades.Add(GetIntBetweenMinMax("Student Grade: ", 0, 100));
+                if (GetString("Add another? y/n: ") == "n")
+                    adding = false;
+            }
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                Console.WriteLine($"Name: {studentNames[i]}, Grade: {studentGrades[i]}");
+            }
+        }
         public void App(string demoName)
         {
             try
             {
                 Console.WriteLine($"{demoName} started");
-                
+                WithArrays();
+                WithLists();
                 Console.WriteLine($"{demoName} ended");
                 Console.WriteLine("");
             }
